64a5faff50fa38aeb36125e58eef58f4359a4fd8
Emily Briaguez-Agurto <emilyb.agurto@gmail.com>
2022-08-30 15:45:41 -0400

    classwork completed

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    //'psvm' shortcut for code below
+    public static void main(String[] args) {
+        //'sout' shortcut for code below
+        System.out.println("what's up");
+    }
+}
+
+//create an intellij maven project
+//create a java class filed named person
+//put a public static void main inside
+//print out 'I got this shit!'
